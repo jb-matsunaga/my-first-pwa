d2cc26f6627ec0cf04490a4edb3ee1b13940f677
jb-matsunaga <jb.matsunaga@gmail.com>
2017-06-20 00:58:54 +0900

    third commit

---
+++ final/service-worker.js
@@ -0,0 +1,113 @@
+// Copyright 2016 Google Inc.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+const dataCacheName = 'weatherData-v1';
+
+
+
+const cacheName = 'weatherPWA-final-1';
+
+const filesToCache = [
+  '/',
+  '/index.html',
+  '/scripts/app.js',
+  '/styles/inline.css',
+  '/images/clear.png',
+  '/images/cloudy-scattered-showers.png',
+  '/images/cloudy.png',
+  '/images/fog.png',
+  '/images/ic_add_white_24px.svg',
+  '/images/ic_refresh_white_24px.svg',
+  '/images/partly-cloudy.png',
+  '/images/rain.png',
+  '/images/scattered-showers.png',
+  '/images/sleet.png',
+  '/images/snow.png',
+  '/images/thunderstorm.png',
+  '/images/wind.png'
+];
+
+// installイベントは一度だけ発生
+self.addEventListener('install', (e) => {
+  console.log('[ServiceWorker] Install');
+  /* waitUntil()メソッドは、Promiseオブジェクトを受け取る。
+   * 処理がresolveに至るまでServiceWorkerが次の状態へ進まないことを保証。
+   * Promiseが解決するまでinstalling状態のままで待機。
+   */
+  e.waitUntil(
+    // Cacheを取得、なければ新しく作成
+    caches.open(cacheName).then((cache) => {
+      console.log('[ServiceWorker] Caching app shell');
+      // filesToCacheをキャッシュさせる
+      return cache.addAll(filesToCache);
+    })
+  );
+});
+
+// install完了後、activateイベントが続く
+self.addEventListener('activate', (e) => {
+  console.log('[ServiceWorker] Activate');
+  // Promiseが解決するまでactivating状態のままで待機
+  e.waitUntil(
+    caches.keys().then((keyList) => {
+      return Promise.all(keyList.map((key) => {
+        if (key !== cacheName) {
+          console.log('[ServiceWorker] Removing old cache', key);
+          return caches.delete(key);
+        }
+      }));
+    })
+  );
+  /*
+   * Fixes a corner case in which the app wasn't returning the latest data.
+   * You can reproduce the corner case by commenting out the line below and
+   * then doing the following steps: 1) load app for first time so that the
+   * initial New York City data is shown 2) press the refresh button on the
+   * app 3) go offline 4) reload the app. You expect to see the newer NYC
+   * data, but you actually see the initial data. This happens because the
+   * service worker is not yet activated. The code below essentially lets
+   * you activate the service worker faster.
+   */
+  return self.clients.claim();
+});
+
+
+self.addEventListener('fetch', (e) => {
+  console.log('ブラウザからのリクエストをキャッチ');
+  // お天気API
+  const dataUrl = 'https://query.yahooapis.com/v1/public/yql';
+  if (e.request.url.indexOf(dataUrl) > -1) {
+    /* リクエストURLにdataUrlが含まれている場合、アプリは最新の気象データを要求。
+     * 常にネットワークに行き、応答をキャッシュする。
+     */
+    e.respondWith(
+      // Cacheを取得、なければ新しく作成
+      caches.open(dataCacheName).then((cache) => {
+        // 通常のリクエストを返す
+        return fetch(e.request).then((response) => {
+          cache.put(e.request.url, response.clone());
+          return response;
+        });
+      })
+    );
+  } else {
+    e.respondWith(
+      // リクエストに対応するキャッシュがあるかどうか調査
+      caches.match(e.request).then((response) => {
+      // キャッシュがあったらresponseを返す、なかったら通常のリクエスト
+        return response || fetch(e.request);
+      })
+    );
+  }
+});
